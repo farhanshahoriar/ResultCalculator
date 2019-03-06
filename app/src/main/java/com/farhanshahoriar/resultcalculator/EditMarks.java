932e59e83b3e2caf1b1ac256f06dded97b9c745b
S.M. Farhan Shahoriar <farhanshahoriar@gmail.com>
2019-03-07 01:01:26 +0600

    Some activity aded

---
+++ app/src/main/java/com/farhanshahoriar/resultcalculator/EditMarks.java
@@ -0,0 +1,13 @@
+package com.farhanshahoriar.resultcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditMarks extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_marks);
+    }
+}
--- app/src/main/java/com/farhanshahoriar/resultcalculator/MainActivity.java
+++ app/src/main/java/com/farhanshahoriar/resultcalculator/MainActivity.java
@@ -1,7 +1,12 @@
 package com.farhanshahoriar.resultcalculator;
 
+import android.content.Intent;
+import android.support.annotation.VisibleForTesting;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    void onClkClassOnResult(View view){
+
+        Toast.makeText(this,"BtnOne",Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this,DisplayResult.class);
+        intent.putExtra("Class","one");
+        startActivity(intent);
+
     }
 }
